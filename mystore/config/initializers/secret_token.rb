82ad9c8b0be314dfdc7d5c3cf3926fb47ac2feef
Jack Guan <gjj@cs.dartmouth.edu>
2014-01-29 09:53:49 -0500

    successfully install spree though it tooks me hours and to install and configuration the fancy_spree, it is an awesome framework, so far

---
+++ mystore/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mystore::Application.config.secret_key_base = 'd1546afb3460be78bbf082a2dc1ccc27693f81be00aad970c2ba546495aae3e7f54e5223cf496d89cc900f8e8e2f1dca8a9903d267cc0d8e49179fac5b8f0bcc'
